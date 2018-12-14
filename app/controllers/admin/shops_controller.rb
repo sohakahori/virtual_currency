bc72dc42d3fb4c1b98f92bdb557eb90e2304c05f
sohakahori <sohakahori@gmail.com>
2018-12-14 17:59:10 +0900

    管理画面：取引所作成

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -2,4 +2,24 @@ class Admin::ShopsController < Admin::ApplicationController
   def index
     @shops = Shop.page(params[:page]).per(PER_PAGE)
   end
+
+  def new
+    @shop = Shop.new()
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.save
+      flash[:success] = "取引所を登録しました"
+      redirect_to admin_shops_path
+    else
+      flash.now[:danger] = "不正な入力値です"
+      render :new
+    end
+  end
+
+  private
+  def shop_params
+    params.require(:shop).permit(:name, :address, :company, coin_ids: [])
+  end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,9 @@ class Shop < ApplicationRecord
   # アソシエーション
   has_many :coin_shops, dependent: :destroy
   has_many :coins, through: :coin_shops
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :company, presence: true
+  validates :coin_shops, presence: true
 end
