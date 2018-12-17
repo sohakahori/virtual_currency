7c9661e8724ed004f5bafe3be1c6c6891713e16a
sohakahori <sohakahori@gmail.com>
2018-12-17 16:38:22 +0900

    管理画面：取引所詳細・更新

--- app/controllers/admin/shops_controller.rb
+++ app/controllers/admin/shops_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ShopsController < Admin::ApplicationController
+
+  before_action :get_shop, only: [:show, :edit, :update]
+
   def index
     @shops = Shop.page(params[:page]).per(PER_PAGE)
   end
@@ -18,8 +21,30 @@ class Admin::ShopsController < Admin::ApplicationController
     end
   end
 
+  def show
+  end
+
+  def eidt
+  end
+
+  def update
+    CoinShop.destroy(@shop.coin_shops.ids)
+    if @shop.update(shop_params)
+      flash[:success] = "取引所を更新しました"
+      redirect_to admin_shop_path
+    else
+      flash.now[:danger] = "不正な入力値です"
+      render :edit
+    end
+  end
+
+
   private
   def shop_params
     params.require(:shop).permit(:name, :address, :company, coin_ids: [])
+  end
+
+  def get_shop
+    @shop = Shop.find(params[:id])
   end
 end
