4f806e61c50c76fe1aa7522fad567ce36c7abbaf
sohakahori <sohakahori@gmail.com>
2018-12-25 13:21:25 +0900

    Userモデルバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true
 end
