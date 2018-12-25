29ddeb9b36ed2329a4b8e6d128ef911ce5e83fc1
sohakahori <sohakahori@gmail.com>
2018-12-25 13:39:09 +0900

    モデルテスト(User)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:first_name) { |n| "first_name#{n}"}
+    sequence(:last_name) { |n| "last_name#{n}"}
+    sequence(:nickname) { |n| "nickname#{n}"}
+    sequence(:email) { |n| "test#{n}@test.com"}
+    password "testtest"
   end
 end
