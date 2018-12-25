1a8e5a5898c663bdc8a34becc57b13ab94793ffe
sohakahori <sohakahori@gmail.com>
2018-12-26 05:59:59 +0900

    public：取引所詳細画面

--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -2,4 +2,8 @@ class Public::ShopsController < Public::ApplicationController
   def index
     @shops = Shop.page(params[:page]).per(PER_PAGE)
   end
+
+  def show
+    @shop = Shop.find(params[:id])
+  end
 end
