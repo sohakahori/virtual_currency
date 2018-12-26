36ffaeffb7a85b9366ec4b4f20ddbac6b4c02174
sohakahori <sohakahori@gmail.com>
2018-12-26 15:47:07 +0900

    コントローラテスト(public/shop#index)

--- spec/controllers/public/shops_controller_spec.rb
+++ spec/controllers/public/shops_controller_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Public::ShopsController, type: :controller do
 
+  let(:user) { FactoryBot.create(:user) }
+  describe 'GET #index' do
+    context "認証済み" do
+      before do
+        sign_in user
+      end
+      it "200ステータスコードをレスポンスする" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "未認証" do
+      it "200ステータスコードをレスポンスする" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
