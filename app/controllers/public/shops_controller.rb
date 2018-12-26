f2fd9e6483b19a859f1a1eaf248536630d00dc6d
sohakahori <sohakahori@gmail.com>
2018-12-26 14:23:57 +0900

    public：取引所一覧検索処理追加

--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -1,9 +1,33 @@
 class Public::ShopsController < Public::ApplicationController
   def index
-    @shops = Shop.page(params[:page]).per(PER_PAGE)
+    @q = params[:q]
+    @coin_ids = params[:coin_ids]
+    @coins = Coin.all
+    @shops = Shop.eager_load(:coin_shops).eager_load(:coins)
+
+    if @q.present?
+      @shops = search_shops_prosess
+    end
+
+    if @coin_ids.present?
+      @shops = search_coins_prosess
+    end
+
+    @shops = @shops.page(params[:page]).per(PER_PAGE)
   end
 
   def show
     @shop = Shop.find(params[:id])
+  end
+
+  private
+  def search_shops_prosess
+    @shops.search_name(@q)
+        .or(@shops.search_address(@q))
+        .or(@shops.search_company(@q))
+  end
+
+  def search_coins_prosess
+    @shops.merge(Coin.coin_ids(@coin_ids))
   end
 end
--- app/helpers/public/shops_helper.rb
+++ app/helpers/public/shops_helper.rb
@@ -1,2 +1,5 @@
 module Public::ShopsHelper
+  def get_coin_check_box_tag coin, coin_ids
+    Array(coin_ids).include?(coin.id.to_s) ? check_box_tag("coin_ids[]", coin.id, checked: true) : check_box_tag("coin_ids[]", coin.id)
+  end
 end
