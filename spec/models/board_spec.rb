8a0c5b7ed6f5610d6ed06f090bda22a110388903
sohakahori <sohakahori@gmail.com>
2018-12-27 21:15:13 +0900

    featureテスト(public/boards#create)

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Board, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "バリデーション適用" do
+    it "titleが未入力の時はバリデーションが適用される" do
+      board = FactoryBot.build(:board, title: nil)
+      board.valid?
+      expect(board.errors.messages[:title]).to include("を入力してください")
+    end
+
+    it "titleが21字以上入力時はバリデーションが適用される" do
+      board = FactoryBot.build(:board, title: ("a" * 21))
+      board.valid?
+      expect(board.errors.messages[:title]).to include("は20文字以内で入力してください")
+    end
+  end
+
+  context "バリデーション未適用", forcus: true do
+    it "正常な値が入力された時はバリデーションが適用されないこと" do
+      board = FactoryBot.build(:board, title: ("a" * 20))
+      board.valid?
+      expect(board).to be_valid
+    end
+  end
 end
