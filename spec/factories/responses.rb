f47639bef0d3b56aa5982ad8a6e4c7feead0f605
sohakahori <sohakahori@gmail.com>
2018-12-27 21:16:57 +0900

    モデルテスト(response)

--- spec/factories/responses.rb
+++ spec/factories/responses.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :response do
-    
+    sequence(:body) { |n| "response body#{n}"}
+    association :user
+    association :board
   end
 end
--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Response, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "バリデーション適用" do
+    it "bodyが未入力の時はバリデーションが適用される" do
+      response = FactoryBot.build(:response, body: nil)
+      response.valid?
+      expect(response.errors.messages[:body]).to include("を入力してください")
+    end
+
+    it "bodyが51字以上入力時はバリデーションが適用される" do
+      response = FactoryBot.build(:response, body: ("a" * 51))
+      response.valid?
+      expect(response.errors.messages[:body]).to include("は50文字以内で入力してください")
+    end
+  end
+
+  context "バリデーション未適用" do
+    it "正常な値が入力された時はバリデーションが適用されないこと" do
+      response = FactoryBot.build(:response, body: ("a" * 50))
+      response.valid?
+      expect(response).to be_valid
+    end
+  end
 end
