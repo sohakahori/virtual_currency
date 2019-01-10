98e39807cd5106759902d26cf0cac6b3da2c36cc
sohakahori <sohakahori@gmail.com>
2019-01-10 21:07:06 +0900

    リファクタリング(admin/response_controller.rb)

--- app/controllers/admin/responses_controller.rb
+++ app/controllers/admin/responses_controller.rb
@@ -3,9 +3,8 @@ class Admin::ResponsesController < Admin::ApplicationController
   before_action :get_board, only: [:index, :show]
 
   def index
-    @responses = @board.responses.includes(:user)
-    search_process
-    @responses = @responses.page(params[:page]).per(PER_PAGE)
+    @params = params
+    @responses = GetResponsesByBordService.new(params, @board).call
   end
 
   def show
@@ -25,54 +24,8 @@ class Admin::ResponsesController < Admin::ApplicationController
     redirect_to admin_board_responses_path(params[:board_id])
   end
 
+  private
   def get_board
     @board = Board.find(params[:board_id])
-  end
-
-  private # Todo: 要リファクタリング
-  def search_process
-    @q = {}
-    if params[:q].present?
-      @q[:response] = params[:q][:response]
-      @q[:user]     = params[:q][:user]
-    else
-      @q[:response] = nil
-      @q[:user]     = nil
-    end
-
-    if @q[:response].present?
-      search_responses
-    end
-    if @q[:user].present?
-      @responses = @responses.references(:users)
-      search_users
-    end
-  end
-
-  def search_responses
-    q_responses = @q[:response].split(" ").map do |q_response|
-      @responses.search_id(q_response).or(@responses.search_body(q_response))
-    end
-    q_responses.each_with_index do |response, i|
-      if i == 0
-        @responses = response
-      else
-        @responses = @responses.or(response)
-      end
-    end
-  end
-
-  def search_users
-    q_users = @q[:user].split(" ").map do |q_user|
-      @responses.merge(User.search_id(q_user).or(User.search_no_space_full_name(q_user)))
-    end
-
-    q_users.each_with_index do|user, i|
-      if i == 0
-        @responses = user
-      else
-        @responses = @responses.or(user)
-      end
-    end
   end
 end
--- app/helpers/admin/responses_helper.rb
+++ app/helpers/admin/responses_helper.rb
@@ -1,2 +1,9 @@
 module Admin::ResponsesHelper
+  def get_response_param
+    @params[:q].present? && @params[:q][:response].present? ? @params[:q][:response] : nil
+  end
+
+  def get_user_param
+    @params[:q].present? && @params[:q][:user].present? ? @params[:q][:user] : nil
+  end
 end
