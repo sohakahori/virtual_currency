83110821eb72cc15bdceecee01ae9b7155d4bcb5
sohakahori <sohakahori@gmail.com>
2019-01-12 14:11:19 +0900

    リファクタリング

--- app/services/get_shops_service.rb
+++ app/services/get_shops_service.rb
@@ -14,12 +14,8 @@ class GetShopsService
 
   def get_shops
     shops = Shop.eager_load(:coin_shops).eager_load(:coins)
-    if params[:q].present?
-      shops = shops.search_name(params[:q]).or(shops.search_address(params[:q])).or(shops.search_company(params[:q]))
-    end
-    if checked_coin_ids.present?
-      shops = shops.merge(Coin.coin_ids(checked_coin_ids))
-    end
+    shops = shops.search_name(params[:q]).or(shops.search_address(params[:q])).or(shops.search_company(params[:q])) if params[:q].present?
+    shops = shops.merge(Coin.coin_ids(checked_coin_ids)) if checked_coin_ids.present?
     shops.page(params[:page]).per(Admin::ApplicationController::PER_PAGE)
   end
 end
