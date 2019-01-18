fd8fc7fbfb5e81570546f4a39c908e1cf6e23564
sohakahori <sohakahori@gmail.com>
2019-01-18 20:54:21 +0900

    API：コメント一覧(ソース要確認)

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,7 +6,11 @@ class Api::V1::ApplicationController < ActionController::Base
   include Api::V1::Filter::SetQueryStringToHash
 
 
+  PER_PAGE = 30
+
   def render_error code, message, full_messages = nil
     render 'api/v1/base/error', formats: 'json', handlers: 'jbuilder', status: code, locals: { message: message, code: code, full_messages: full_messages }
   end
+
+
 end
